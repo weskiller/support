98482fdb31ef7dbf350cf7335a132fc73036af49
weskiller <weskiller@vip.qq.com>
2020-11-25 16:02:19 +0800

    Add helper

---
+++ src/helper.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Support\Enumerable;
+use Weskiller\Support\Json\Translator;
+
+if (!function_exists('forceToArray')) {
+    /**
+     * @param $item
+     *
+     * @return array|mixed
+     * @throws
+     *
+     */
+    function forceToArray($item)
+    {
+        if (is_array($item)) {
+            return $item;
+        }
+
+        if ($item instanceof Enumerable) {
+            return $item->all();
+        }
+
+        if ($item instanceof Arrayable
+            || method_exists($item,'toArray')
+        ) {
+            return $item->toArray();
+        }
+
+        if ($item instanceof Jsonable
+            || method_exists($item,'toJson')
+        ) {
+            return Translator::decode($item->toJson());
+        }
+
+        if ($item instanceof Enumerable) {
+            return $item->all();
+        }
+
+        if ($item instanceof JsonSerializable) {
+            return (array) $item->jsonSerialize();
+        }
+
+        if ($item instanceof Traversable) {
+            return iterator_to_array($item);
+        }
+
+        return (array) $item;
+    }
+}
